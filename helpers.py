42a7e8f64893bdf488427aa382b9ab722312deb1
Irene Theiss <irene-rudel@gmx.at>
2022-01-28 23:21:19 +0100

    fix datatypes and add continents

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import numpy
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def get_num_countries(datasets, names):
     for i, val in enumerate(datasets):
@@ -11,7 +12,8 @@ def get_missing(data):
     #filter for values < 0 
     filtered = data['missing'][data['missing'] != 0]
     ctries = data['country'][data['missing'] != 0]
-    #return filtered data
+    #drop 'missing' column
+    data.drop(columns=['missing'], inplace=True)
     return (filtered, ctries)
 
 def plot_missing(data, x_values, x_labels, set_name):
@@ -22,3 +24,16 @@ def plot_missing(data, x_values, x_labels, set_name):
     plt.xlabel('Country')
     plt.ylabel('Number of missing values')
     plt.title(('Missing values per country in {} data').format(set_name), fontsize=13, fontweight='bold')
+    
+def cut_suffix_tonumber(x):
+    #check last char, cut last char, convert to float, 
+    #multiply depending if last char is 'M', 'k' or 'B'
+    #if no letter-suffix, just convert to float 
+    if x[-1:] == 'M':
+        return pd.to_numeric(x[:-1]) * 1000000
+    elif x[-1:] == 'k':
+        return pd.to_numeric(x[:-1]) * 1000
+    elif x[-1:] == 'B':
+        return pd.to_numeric(x[:-1]) * 1000000000
+    else:
+        return pd.to_numeric(x[:-1])
